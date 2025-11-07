851f43f819505a32983ba373eae9e71cc2feb7d5
RaykoRadev <christmas898@gmail.com>
2025-11-07 11:09:30 +0100

    dynamically rendering users
    separate the main fail to components, fetching the users and mapping them

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-function App() {
-    const [count, setCount] = useState(0);
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Pagination from "./components/Pagination";
+import Table from "./components/Table";
 
-    return <div></div>;
+function App() {
+    return (
+        <>
+            <Header />
+            <main className="main">
+                <section className="card users-container">
+                    <Table />
+
+                    <button className="btn-add btn">Add new user</button>
+
+                    <Pagination />
+                </section>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
