2be5629a484ad72bf52ebd9ebcd98a28cccfbaf1
RaykoRadev <christmas898@gmail.com>
2025-11-07 11:46:09 +0100

    open/close save modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import SaveUserModal from "./components/CreateUserModal";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Pagination from "./components/Pagination";
 import Table from "./components/Table";
 
 function App() {
+    const [modal, setModal] = useState(false);
+
+    const createClickHandler = () => {
+        setModal(true);
+    };
+
+    const closeModal = () => {
+        setModal(false);
+    };
+
     return (
         <>
             <Header />
@@ -11,10 +23,17 @@ function App() {
                 <section className="card users-container">
                     <Table />
 
-                    <button className="btn-add btn">Add new user</button>
+                    <button
+                        className="btn-add btn"
+                        onClick={createClickHandler}
+                    >
+                        Add new user
+                    </button>
 
                     <Pagination />
                 </section>
+
+                {modal && <SaveUserModal closeModal={closeModal} />}
             </main>
             <Footer />
         </>
